f701ef6f01f7e45a93e5b9ea64c67db7d0501a79
BeniVF <beni.villa@gmail.com>
2020-02-29 11:13:12 +0100

    First approach for process using scala standard library.

---
+++ core/src/main/scala/c4s/process/ProcessResult.scala
@@ -0,0 +1,18 @@
+package c4s.process
+
+import cats.effect.Sync
+import cats.implicits._
+
+import fs2.{Stream, text}
+
+final case class ProcessResult[F[_]](
+  exitCode: Int,
+  output: Stream[F, Byte],
+  error: Stream[F, Byte]
+)
+
+object ProcessResult {
+
+def mkString[F[_]: Sync](stream: Stream[F, Byte]): F[String] =
+  stream.through(text.utf8Decode).compile.toVector.map(_.mkString)
+}
