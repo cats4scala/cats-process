05b0ba26c04d1a89b0f7a192a119acb255e1d789
Benigno Villa Fernández <beni.villa@gmail.com>
2020-03-30 10:15:10 +0200

    Refactor (#37)
    * Simplify api. 
    * Add methods in companion object.
    * Separate Logger.
    * Add syntax to simplify the api.
    * Make the process to run strict.

---
+++ core/src/main/scala/c4s/process/LoggerProcess.scala
@@ -0,0 +1,35 @@
+package c4s.process
+
+import cats.implicits._
+import cats.effect._
+import io.chrisdavenport.log4cats.Logger
+import c4s.process.syntax._
+import java.nio.file.Path
+import fs2.{text, Stream}
+
+private[process] final class LoggerProcess[F[_]: Sync](process: Process[F], logger: Logger[F]) extends Process[F] {
+
+  final def run(command: String, path: Option[Path]): F[ProcessResult[F]] =
+    for {
+      result <- process.run(command, path)
+      newResult <- logProcessResult(path, command, result)
+    } yield newResult
+
+  private def logProcessResult(path: Option[Path], command: String, result: ProcessResult[F]): F[ProcessResult[F]] =
+    for {
+      _ <- logger.info(s"[${result.exitCode}] ${path.fold("")(x => s"$x/")}$command")
+      output <- result.output.asString
+      _ <- if (output.nonEmpty)
+        logger.info(output)
+      else
+        Sync[F].unit
+      error <- result.error.asString
+      _ <- if (error.nonEmpty)
+        logger.error(error)
+      else
+        Sync[F].unit
+    } yield result.copy(
+      output = Stream(output).through(text.utf8Encode),
+      error = Stream(error).through(text.utf8Encode)
+    )
+}
