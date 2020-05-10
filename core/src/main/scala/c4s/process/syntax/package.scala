1e01f86473322f2dd6f6521dcaeb3898b47f664a
JesusMtnez <jesusmartinez93@gmail.com>
2020-05-10 08:55:08 +0200

    Fix format

--- core/src/main/scala/c4s/process/syntax/package.scala
+++ core/src/main/scala/c4s/process/syntax/package.scala
@@ -8,21 +8,23 @@ package object syntax {
 
   implicit class ProcessOps[F[_]: Sync](process: Process[F]) {
     import io.chrisdavenport.log4cats.Logger
+
     /**
-      * Wraps the [[c4s.process.Process]] with a `Logger`.
-      *
-      * @param logger instance to use
-      * @return [[c4s.process.Process]] instance with logging enabled
-      */
+     * Wraps the [[c4s.process.Process]] with a `Logger`.
+     *
+     * @param logger instance to use
+     * @return [[c4s.process.Process]] instance with logging enabled
+     */
     final def withLogger(logger: Logger[F]): Process[F] = new LoggerProcess(process, logger)
   }
 
   implicit class StreamBytesOps[F[_]: Sync](stream: Stream[F, Byte]) {
+
     /**
-      * Transform the `Stream` to `String`
-      *
-      * @return a `String` containing the `Stream` content
-      */
+     * Transform the `Stream` to `String`
+     *
+     * @return a `String` containing the `Stream` content
+     */
     final def asString: F[String] =
       stream
         .through(text.utf8Decode)
@@ -31,28 +33,29 @@ package object syntax {
         .map(_.mkString)
 
     /**
-      * Transform `Stream` to `List[String]`
-      *
-      * @return a `List[String]` containing each `Stream` line as a String
-      */
+     * Transform `Stream` to `List[String]`
+     *
+     * @return a `List[String]` containing each `Stream` line as a String
+     */
     final def asLines: F[List[String]] =
       asString
         .map(_.split('\n').toList)
   }
 
   implicit class ProcessResultOps[F[_]: Sync](result: F[ProcessResult[F]]) {
+
     /**
-      * Transform and return the stdout as a `List[String]`, one per line
-      *
-      * @return process' stdout as a list of `String`
-      */
+     * Transform and return the stdout as a `List[String]`, one per line
+     *
+     * @return process' stdout as a list of `String`
+     */
     final def lines: F[List[String]] = result.flatMap(_.output.asLines)
 
     /**
-      * Transform and return stdout to a `String`
-      *
-      * @return stdout as `String`
-      */
+     * Transform and return stdout to a `String`
+     *
+     * @return stdout as `String`
+     */
     final def string: F[String] = result.flatMap(_.output.asString)
 
     /**
